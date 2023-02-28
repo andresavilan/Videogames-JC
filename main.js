ea276ba345ce2aed795e4e3cdb476cbeb2d78d8d
Andres Avilan <andresfavilan@hotmail.com>
2023-02-28 16:20:33 -0500

    added canvas and responsive design

---
+++ main.js
@@ -0,0 +1,27 @@
+const canvas = document.querySelector('#game')
+const game = canvas.getContext('2d')
+
+window.addEventListener('load', startGame);
+
+function startGame() {
+    let canvasSize;
+
+    if(window.innerWidth<window.innerHeight) {
+        canvasSize = window.innerWidth * 0.8
+    } else {
+        canvasSize = window.innerHeight * 0.8
+    }
+canvas.setAttribute('width', canvasSize)
+canvas.setAttribute('height', canvasSize)
+
+const elementSize = canvasSize /10
+
+game.font = elementSize + 'px Verdana'
+game.textAlign = 'end'
+
+for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis['X'], elementSize * i, elementSize)  
+}
+
+}
+
--- maps.js
+++ maps.js
@@ -9,7 +9,8 @@ const emojis={'-':' ','O':'🚪','X':'💣','I':'🎁','PLAYER':'💀','BOMB_COL
   -XXXXXXXXX
   -XXXXXXXXX
   OXXXXXXXXX
-`);maps.push(`
+`);
+maps.push(`
   O--XXXXXXX
   X--XXXXXXX
   XX----XXXX
@@ -20,7 +21,8 @@ const emojis={'-':' ','O':'🚪','X':'💣','I':'🎁','PLAYER':'💀','BOMB_COL
   XX--XXX-XX
   XXXX---IXX
   XXXXXXXXXX
-  `);maps.push(`
+  `);
+  maps.push(`
   I-----XXXX
   XXXXX-XXXX
   XX----XXXX
